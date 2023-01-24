273d9f52084a720fdf703007bea88950481fd06c
Antoine Lévêque <antoine.leveque@epita.fr>
2023-01-24 12:55:15 +0100

    introduction

--- gen_tree.py
+++ gen_tree.py
@@ -6,4 +6,20 @@ Arbres généraux :
     - T.key = valeur de la clé
     - T.children : liste des sous-arbres
     - T.nbchildren : nombre de fils du noeud T
+
+- lien dans git bash : cd OneDrive\ -\ EPITA/Algorithmique/S3/
+
+    - B : TreeAsBin
+    - B.key : valeur de la clé
+    - B.child : lien vers le premier fils gauche
+    - B.sibling : lien vers le premier frère droit
+    - noeud est une feuille quand B.child == None
 """
+# Partie 1 : Mesures
+
+# Ex 1.1 : La taille
+def size(A):
+    n = 1
+    #pour chaque sous-arbre de Ai (sera le cas d'arrêt si le noeud est une feuille)
+    #   n+=size(Ai)
+    return n
