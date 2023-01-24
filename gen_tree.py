f2e914ddfc2965a1ddd733dae324abd228c71ed0
Antoine Lévêque <antoine.leveque@epita.fr>
2023-01-24 12:39:30 +0100

    first commit

---
+++ gen_tree.py
@@ -0,0 +1,9 @@
+"""
+Arbres généraux :
+    - A = <r,liste d'arbres>
+    - A = <r,{A0,...,An}>
+    - T : l'arbre T
+    - T.key = valeur de la clé
+    - T.children : liste des sous-arbres
+    - T.nbchildren : nombre de fils du noeud T
+"""
