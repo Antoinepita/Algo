3084945fba74dfef11e17a76e8be829810076121
Antoinepita <antoine.leveque@epita.fr>
2023-02-09 18:51:40 +0100

    rajout des fonctions dicho et search (travail)

--- B_trees.py
+++ B_trees.py
@@ -1,4 +1,4 @@
-from algo_py import  btree
+#from algo_py import  btree
 
 """
 Arbres-B :
@@ -31,11 +31,12 @@ Ex 1.1 :
 # Ex 1.3 : Liste des éléments en ordre croissant d'un arbre
 def _build(B,L):
     if B.children==[]:  # cas feuille
-        for i in range(len(B.keys)):
+        for elt in B.keys:
             L.append(B.keys[i])
     else:
         for i in range(B.nbkeys):   # len(B.children)-1
             _build(B.children[i],L)
+            L.append(B.keys[i])
         _build(B.children[B.nbkey],L)   # B.children[-1]
             
 
@@ -43,3 +44,55 @@ def btree2list(B):
     L = []
     _build(B,L)
     return L
+
+# Partie 2 : Classiques
+
+"""
+Ex 2.1 : Minimum et maximum
+1) La valeur minimale correspond à la première clé de la feuille la plus à gauche et la valeur maximale est la dernière clé de la feuille la plus à droite
+"""
+
+def getMin(B):
+    C = B
+    while C.nbkeys!=0:  # bord gauche
+        C = C.children[0]
+    return C.keys[0]    # valeur de la première clé
+
+def getMax(B):
+    C = B
+    while C.nbkeys!=0:  # bord droit
+        C = C.children[-1]
+    return C.keys[-1]   # valeur de la dernière clé
+
+# Ex 2.2: Recherche d'un élément
+def dicho(L,x):
+    """
+    renvoie la place de l'élément x dans la liste triée en ordre croissant L
+    renvoie l'index juste avant lequel il devrait être inséré s'il n'y est pas
+    """
+    a = 0
+    b = len(L)-1
+    m = (a+b)//2
+    while a < b :
+        if L[m] ==  x:
+            return m
+        elif L[m] > x:
+            b = m-1
+        else :
+            a = m+1
+        m = (a+b)//2
+    return a
+
+print(dicho([0,1,2,3,4,5,6,7,8,9],))
+
+def search(B,x):
+    i = dicho(B.keys,x)
+    if B.keys[i]==x:    # cas d'arrêt : valeur trouvée
+        return (B,i)
+    if B.keys[i]!=x and (B.keys[i]).children==0:    # cas d'arrêt : valeur pas trouvée dans la liste et pas d'enfant donc n'est pas dans l'arbre
+        return None
+    else:
+        if x>B.keys[i]: # sinon on réeffectue la recherche sur :
+            search(B.children[i+1],x) # le sous arbre droit de là où il devrait se trouver si x>clé actuelle 
+        else:
+            search(B.children[i],x) # le sous-arbre gauche sinon (x<=valeur de la clé actuelle)
